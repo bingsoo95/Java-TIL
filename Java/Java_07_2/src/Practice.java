73192e330a0bce60f40d9f4671984d44204cf84e
bingsoo95 <kimminsu403@naver.com>
2023-03-10 23:49:57 +0900

    class

--- Java/Java_07_2/src/Practice.java
+++ Java/Java_07_2/src/Practice.java
@@ -3,10 +3,17 @@
 
 class Calculator {
 
-    public int sum(int a, int b) {
+    public int sum(int a, int b) { return a + b; }
+    public double sum(double a, double b) {
         return a + b;
     }
+    public  double sum(String a, String b) {
+        return  Double.parseDouble(a) + Double.parseDouble(b);
+    }
 
+    public  int sum(int a, int b, int c) {
+        return a + b + c;
+    }
 }
 
 public class Practice {
@@ -14,8 +21,8 @@ public class Practice {
         // Test code
         Calculator c = new Calculator();
         System.out.println(c.sum(1, 2));
-//        System.out.println(c.sum(1.0, 2.0));
-//        System.out.println(c.sum("1", "2"));
-//        System.out.println(c.sum(1, 2, 3));
+        System.out.println(c.sum(1.0, 2.0));
+        System.out.println(c.sum("1", "2"));
+        System.out.println(c.sum(1, 2, 3));
     }
 }
