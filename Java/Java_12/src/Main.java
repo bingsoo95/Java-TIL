f866c23bf58ae30d63fbd605e43e914cce26f292
bingsoo95 <kimminsu403@naver.com>
2023-03-16 21:25:08 +0900

    IOException

--- Java/Java_12/src/Main.java
+++ Java/Java_12/src/Main.java
@@ -11,6 +11,22 @@ class  Outer {
             Outer.this.print();
         }
     }
+
+    static class InnerStaticClass {
+        void innerPrint() {
+//            Outer.this.print(); // static이 붙어 외부에서 바로 접근 불가
+        }
+    }
+}
+
+abstract class Person {
+    public abstract void printInfo();
+}
+
+class Student extends Person {
+    public void printInfo() {
+        System.out.println("Student.printInfo");
+    }
 }
 
 public class Main {
@@ -18,13 +34,21 @@ public class Main {
     public static void main(String[] args) {
 
 //      외부 클래스
+        Outer o1 = new Outer();
 
 //      내부 클래스 - 인스턴스
+        Outer.Inner i1 = new Outer().new Inner();
 
 //      내부 클래스 - 정적
+        Outer.InnerStaticClass is1 = new Outer.InnerStaticClass();
 
 //      익명 클래스
-
+        Person p1 = new Person() {
+            @Override
+            public void printInfo() {
+                System.out.println("Main.printInfo");
+            }
+        };
 
     }
 
