1f77a60417cd4aaeb1fd23e11b76695c219e90e1
bingsoo95 <kimminsu403@naver.com>
2023-03-21 00:51:22 +0900

    Java_Practice

--- Java/Java_18_3/src/Practice1.java
+++ Java/Java_18_3/src/Practice1.java
@@ -1,7 +1,17 @@
 
 public class Practice1 {
     public static void solution(int[] nums) {
-
+        int idx = 0;
+        for (int num: nums) {
+            if (idx == 0 || num > nums[idx - 1]) {
+                nums[idx++] = num;
+            }
+        }
+        System.out.print("[" + idx + "] ");
+        for (int i = 0; i < idx; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
