063e01ad853b3009fc88021f87a404d5560df6ab
bingsoo95 <kimminsu403@naver.com>
2023-03-21 22:28:40 +0900

    Array

--- DataStructure/LinearStructure/Practice/LinearDS_03/src/Practice7.java
+++ DataStructure/LinearStructure/Practice/LinearDS_03/src/Practice7.java
@@ -15,7 +15,31 @@
 
 
 public class Practice7 {
-    public static void main(String[] args) {
+    static void printArr(int[][] arr) {
+        for (int[] item10: arr) {
+            for (int item: item10) {
+                System.out.print(item + " ");
+            }
+            System.out.println();
+        }
+    }
 
+    public static void main(String[] args) {
+        int[][] arr = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}};
+
+        int[][] arr90 = new int[arr[0].length][arr.length];
+
+
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                int r = arr.length - 1 - i;
+                arr90[j][r] = arr[i][j];
+            }
+        }
+
+        printArr(arr);
+        System.out.println("== After ==");
+        printArr(arr90);
     }
 }
