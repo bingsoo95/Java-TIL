071c3de3fa3001f3dee10a665f842c084b9b7896
bingsoo95 <kimminsu403@naver.com>
2023-03-24 15:43:05 +0900

    Deque_Palindrome

--- DataStructure/LinearStructure/Practice/LinearDS_11/src/Practice2.java
+++ DataStructure/LinearStructure/Practice/LinearDS_11/src/Practice2.java
@@ -14,9 +14,29 @@
 // 결과: false
 
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Practice2 {
     public static boolean checkPalindrome(String str) {
-        return false;
+        Deque deque = new ArrayDeque();
+        boolean isFront = true;
+        boolean isPalindrome = true;
+
+        for (String s: str.split("")) {
+            deque.addFirst(s);
+        }
+
+        while (!deque.isEmpty()) {
+            String s1 = (String)deque.pollFirst();
+            String s2 = (String)deque.pollLast();
+
+            if (s1 != null && s2 != null && !s1.equals(s2)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        return isPalindrome;
     }
 
     public static void main(String[] args) {
