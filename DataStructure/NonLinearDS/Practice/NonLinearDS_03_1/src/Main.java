e825e1d14429739b843b36330f9799b6aa56c5b2
bingsoo95 <kimminsu403@naver.com>
2023-03-29 00:23:50 +0900

    BST

--- DataStructure/NonLinearDS/Practice/NonLinearDS_03_1/src/Main.java
+++ DataStructure/NonLinearDS/Practice/NonLinearDS_03_1/src/Main.java
@@ -23,11 +23,106 @@ class BinarySearchTree {
     }
 
     public void addNode(int key) {
+        if (this.head == null) {
+            this.head = new Node(key, null, null);
+        } else {
+            Node cur = this.head;
 
+            while (true) {
+                Node pre = cur;
+
+                if (key < cur.key) {
+                    cur = cur.left;
+
+                    if (cur == null) {
+                        pre.left = new Node(key, null, null);
+                        break;
+                    }
+                } else {
+                    cur = cur.right;
+
+                    if (cur == null) {
+                        pre.right = new Node(key, null, null);
+                        break;
+                    }
+                }
+            }
+        }
     }
 
     public void removeNode(int key) {
+        Node parent = null;
+        Node successor = null;
+        Node predecessor = null;
+        Node child = null;
 
+        Node cur = this.head;
+        while (cur != null) {
+            if (key == cur.key) {
+                break;
+            }
+            parent = cur;
+            if (key < cur.key) {
+                cur = cur.left;
+            } else {
+                cur = cur.right;
+            }
+        }
+
+        if (cur == null) {
+            System.out.println("key에 해당하는 노드가 없습니다.");
+            return;
+        }
+
+        if (cur.left == null && cur.right == null) { // Leaf 노드인 경우
+            if (parent == null) {
+                this.head = null;
+            } else {
+                if (parent.left == cur) {
+                    parent.left = null;
+                } else {
+                    parent.right = null;
+                }
+            }
+        } else if (cur.left != null && cur.right == null || cur.left == null && cur.right != null) { // 자식노드가 하나인 경우
+            if (cur.left != null) {
+                child = cur.left;
+            } else {
+                child = cur.right;
+            }
+
+            if (parent == null) {
+                this.head = child;
+            } else {
+                if (parent.left == cur) {
+                    parent.left = child;
+                } else {
+                    parent.right = child;
+                }
+            }
+        } else {    // 자식이 둘인 경우
+            predecessor = cur;
+            successor = cur.left;
+
+            while (successor.right != null) {
+                predecessor = successor;
+                successor = successor.right;
+            }
+
+            predecessor.right = successor.left;
+            successor.left = cur.left;
+            successor.right = cur.right;
+
+            if (parent == null) {
+                this.head = successor;
+            } else {
+                if (parent.left == cur) {
+                    parent.left = successor;
+                } else {
+                    parent.right = successor;
+                }
+            }
+        }
     }
 
     public void levelOrder(Node node) {
