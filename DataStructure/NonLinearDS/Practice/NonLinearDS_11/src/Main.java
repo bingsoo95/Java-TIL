79c5cc5000bb2148251071dd72acd3846c52c973
bingsoo95 <kimminsu403@naver.com>
2023-04-04 23:31:28 +0900

    Trie

--- DataStructure/NonLinearDS/Practice/NonLinearDS_11/src/Main.java
+++ DataStructure/NonLinearDS/Practice/NonLinearDS_11/src/Main.java
@@ -1,11 +1,60 @@
 // 비선형 자료구조 - 트라이 (Trie)
 
+import java.util.HashMap;
+
 class Node {
+    HashMap<Character, Node> child;
+    boolean isTerminal;
+
+    public Node() {
+        this.child = new HashMap<>();
+        this.isTerminal = false;
+    }
 
 }
 
 class Trie {
+    Node root;
+    public Trie() {
+        this.root = new Node();
+    }
 
+    public void insert(String str) {
+        Node cur = this.root;
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            cur.child.putIfAbsent(c, new Node()); // putIfAbsent : c로 시작하는 키가 없으면 데이터를 추가하고 있으면 pass
+            cur = cur.child.get(c);
+
+            if (i == str.length() - 1) {
+                cur.isTerminal = true;
+                return;
+            }
+        }
+    }
+
+    public boolean search(String str) {
+        Node cur = this.root;
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if (cur.child.containsKey(c)) {
+                cur = cur.child.get(c);
+            } else {
+                return false;
+            }
+
+            if (i == str.length() - 1) {
+                if (cur.isTerminal) {
+                    return false;
+                }
+            }
+        }
+        return  true;
+    }
 }
 
 public class Main {
