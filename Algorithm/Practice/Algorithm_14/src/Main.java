5fb787a102007d5625ed81757301dfeac91f02c5
bingsoo95 <kimminsu403@naver.com>
2023-04-10 20:41:55 +0900

    BackTracking

--- Algorithm/Practice/Algorithm_14/src/Main.java
+++ Algorithm/Practice/Algorithm_14/src/Main.java
@@ -2,9 +2,41 @@
 
 public class Main {
 
-    public static int nQueen(int row) {
+    static int n = 4;
+    static int[] board = new int[n];
+    static int cnt;
 
-        return 0;
+    public static int nQueen(int row) {
+        if (row == n) {
+            cnt++;
+            for (int i = 0; i < n; i++) {
+                System.out.print(board[i] + " ");
+            }
+            System.out.println();
+            return cnt;
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            board[row] = i;
+
+            // promising
+            if (isPromising(row)) {
+                nQueen(row + 1);
+            }
+
+        }
+
+        return cnt;
+    }
+
+    public static boolean isPromising(int row) {
+        for (int i = 0; i < row; i++) {
+            if (board[row] == board[i] || row - i == Math.abs(board[row] - board[i])) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
