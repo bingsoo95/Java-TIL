b1ce4cd066cbc22e5ce786133729bd04f41444cf
bingsoo95 <kimminsu403@naver.com>
2023-04-10 17:15:22 +0900

    DP

--- Algorithm/Practice/Algorithm_12/src/Main.java
+++ Algorithm/Practice/Algorithm_12/src/Main.java
@@ -1,16 +1,42 @@
 // 알고리즘 - 다이나믹 프로그래밍
 
 public class Main {
+    // 피보나치 수열 (일반 풀이 - O(n^2)
+    // 계산했던 부분도 다시 계산
     public static int fib(int n) {
-        return 0;
+        if (n <= 1) {
+            return n;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+        }
     }
 
+    // 피보나치 수열 (DP 풀이 - 타뷸레이션 - O(n))
     public static int fibDP(int n) {
-        return 0;
+        int[] dp = new int[n < 2 ? 2 : n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
     }
 
+    // 피보나치 수열 (DP 풀이 - 메모이제이션 - O(n))
+    static int[] dp = new int[8];
     public static int fibDP2(int n) {
-        return 0;
+        if (n <= 2) {
+            return 1;
+        }
+
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+
+        dp[n] = fibDP2(n - 1) + fibDP2(n - 2);
+        return dp[n];
     }
 
     public static void main(String[] args) {
