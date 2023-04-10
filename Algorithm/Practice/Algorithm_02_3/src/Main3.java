a600ca1b1850c3d142c099be0cccda12018bc029
bingsoo95 <kimminsu403@naver.com>
2023-04-10 09:31:15 +0900

    Sort

--- Algorithm/Practice/Algorithm_02_3/src/Main3.java
+++ Algorithm/Practice/Algorithm_02_3/src/Main3.java
@@ -5,7 +5,23 @@ import java.util.Arrays;
 public class Main3 {
 
     public static void shellSort(int[] arr) {
+        int gap = arr.length / 2;
 
+        for (int g = gap; g > 0; g /= 2) {
+            for (int i = g; i < arr.length; i++) {
+                int tmp = arr[i];
+
+                int j = 0;
+                for (j = i - g; j >= 0; j -= g) {
+                    if (arr[j] > tmp) {
+                        arr[j + g] = arr[j];
+                    } else {
+                        break;
+                    }
+                }
+                arr[j + g] = tmp;
+            }
+        }
     }
 
     public static void main(String[] args) {
