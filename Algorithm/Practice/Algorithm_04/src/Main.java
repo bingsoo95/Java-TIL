95e4f75383094ebb1e806aa1f955554b90b17542
bingsoo95 <kimminsu403@naver.com>
2023-04-10 09:59:07 +0900

    BinarySearch

--- Algorithm/Practice/Algorithm_04/src/Main.java
+++ Algorithm/Practice/Algorithm_04/src/Main.java
@@ -3,14 +3,39 @@
 public class Main {
     // 반복문 구조
     public static int binarySearch(int arr[], int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (target == arr[mid]) {
+                return mid;
+            } else if (target < arr[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
 
         return -1;
     }
     
     // 재귀 호출 구조
     public static int binarySearch2(int[] arr, int target, int left, int right) {
-        
-        return -1;
+        if (left > right) {
+            return -1;
+        }
+
+        int mid = (left + right) / 2;
+
+        if (target == arr[mid]) {
+            return mid;
+        } else if (target < arr[mid]) {
+            return binarySearch2(arr, target, left, mid - 1);
+        } else {
+            return binarySearch2(arr, target, mid + 1, right);
+        }
     }
 
     public static void main(String[] args) {
